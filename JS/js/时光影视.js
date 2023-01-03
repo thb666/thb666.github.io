1b91f723b8c03f0f7bbb2e32c83351a9e692bd17
thb666 <371928335@qq.com>
2023-01-03 20:34:08 +0800

    thb

--- JS/js/时光影视.js
+++ JS/js/时光影视.js
@@ -1,25 +1,15 @@
 var rule = {
-    title: '九酷',
-    host: 'https://jiuku.site',
-    class_name:'电影&电视剧&综艺&动漫',
-    class_url:'1&2&3&4',
+    title: '时光影视',
+    host: 'http://www.shigys.com',
+    class_name:'电影&电视剧&综艺&动漫&哔哩哔哩&独家',
+    class_url:'1&2&3&4&40&41',
     homeUrl: '',
     searchUrl: '/index.php/ajax/suggest?mid=1&wd=**',
     searchable: 2,
     quickSearch: 0,
     headers:{'User-Agent':'MOBILE_UA'},
     // 分类链接fypage参数支持1个()表达式
-    // url: '/index.php/api/vod#type=fyclass&page=fypage',
-	url: '/index.php/api/vod#type=fyfilter&page=fypage',
-	filterable:1,//是否启用分类筛选,
-	filter_url:'{{fl.cateId}}',
-	filter: {"1":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"1"},{"n":"动作片","v":"6"},{"n":"喜剧片","v":"7"},{"n":"爱情片","v":"8"},{"n":"科幻片","v":"9"},{"n":"恐怖片","v":"10"},{"n":"剧情片","v":"11"},{"n":"战争片","v":"12"}]}],"2":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"2"},{"n":"国产剧","v":"13"},{"n":"港台剧","v":"14"},{"n":"日韩剧","v":"15"},{"n":"欧美剧","v":"16"}]}]},
-	filter_def:{
-		1:{cateId:'1'},
-		2:{cateId:'2'},
-		3:{cateId:'3'},
-		4:{cateId:'4'}
-	},
+    url: '/index.php/api/vod#type=fyclass&page=fypage',
     detailUrl:'/index.php/vod/detail/id/fyid.html',
     推荐:'.list-vod.flex .public-list-box;a&&title;.lazy&&data-original;.public-list-prb&&Text;a&&href',
     一级:'',
@@ -29,9 +19,9 @@ var rule = {
 		"img":".detail-pic&&data-original",
 		"desc":".fraction&&Text;.slide-info-remarks:eq(1)&&Text;.slide-info-remarks:eq(2)&&Text;.slide-info:eq(2)--strong&&Text;.slide-info:eq(1)--strong&&Text",
 		"content":"#height_limit&&Text",
-		"tabs":".anthology&&.swiper-slide",
-		"tab_text":".swiper-slide--i&&Text",
-		"lists":".anthology-list-box:eq(#id) li"
+		"tabs":".anthology.wow.fadeInUp.animated&&.swiper-wrapper&&a",
+		"tab_text":".swiper-slide&&Text",
+		"lists":".anthology-list-box:eq(#id)&&.anthology-list-play&&li"
 	},
     搜索:'json:list;name;pic;;id',
 }
